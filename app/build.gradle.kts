74011be027f1c246ab86c58a00a57d63a4e7ae9b
alefdyer <alexeykrasilnikov76@gmail.com>
2024-12-30 09:47:53 +0300

    1.1.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.asinosoft.vpn"
         minSdk = 24
         targetSdk = 35
-        versionCode = 23
+        versionCode = 24
         versionName = "1.1"
         setProperty("archivesBaseName", "$applicationId@$versionName.$versionCode")
         vectorDrawables {
